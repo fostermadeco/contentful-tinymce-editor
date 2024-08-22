378c2b9362e941389da1a42b35a6be713256c090
Kelly Packer <kelly@kellypacker.com>
2024-08-22 15:55:34 -0600

    Add image buttons

--- src/locations/ConfigScreen.tsx
+++ src/locations/ConfigScreen.tsx
@@ -1,16 +1,24 @@
-import React, { useCallback, useState, useEffect } from 'react';
+import React, { useCallback, useState, useEffect, ChangeEvent } from 'react';
 import { ConfigAppSDK } from '@contentful/app-sdk';
 import { Heading, Form, Paragraph, Flex, FormControl, TextInput } from '@contentful/f36-components';
 import { css } from 'emotion';
 import { /* useCMA, */ useSDK } from '@contentful/react-apps-toolkit';
+import {
+  defaultParameters,
+  AppInstallationParameters,
+} from "./ConfigDefaults";
 
-export interface AppInstallationParameters {
-  tinyMceApiKey: string;
-}
+
+
+type ParameterKeys = keyof AppInstallationParameters;
+
 
 const ConfigScreen = () => {
-  const [parameters, setParameters] = useState<AppInstallationParameters>({});
   const sdk = useSDK<ConfigAppSDK>();
+  const [parameters, setParameters] = useState<AppInstallationParameters>({
+    ...defaultParameters,
+    ...sdk.parameters.installation
+  });
   /*
      To use the cma, inject it as follows.
      If it is not needed, you can remove the next line.
@@ -58,6 +66,15 @@ const ConfigScreen = () => {
     })();
   }, [sdk]);
 
+  const onInputChange = (event: ChangeEvent): void => {
+    const target = event.target as HTMLInputElement;
+    const { name, value } = target;
+    const newParams: any = { ...parameters };
+    newParams[name as ParameterKeys] = value;
+
+    setParameters(newParams);
+  };
+
   return (
     <Flex flexDirection="column" className={css({ margin: '80px', maxWidth: '800px' })}>
       <Form>
@@ -79,6 +96,64 @@ const ConfigScreen = () => {
               })
             }}
           />
+          <FormControl.Label>Plugins</FormControl.Label>
+          <TextInput
+            name="plugins"
+            id="plugins"
+            isRequired
+            value={parameters?.plugins}
+            onChange={onInputChange}
+          // textLinkProps={{
+          //   icon: "ExternalLink",
+          //   text: "Plugins",
+          //   onClick: () =>
+          //     window.open("https://www.tiny.cloud/docs/plugins/"),
+          // }}
+          />
+
+          <FormControl.Label>Toolbar</FormControl.Label>
+          <TextInput
+            name="toolbar"
+            id="toolbar"
+            isRequired
+            value={parameters.toolbar}
+            onChange={onInputChange}
+          />
+          <FormControl.HelpText>
+            Use '|' as a toolbar divider. There are 4 toolbar groups: 'formatgroup', 'assetgroup', 'insertgroup' and 'extragroup'.
+          </FormControl.HelpText>
+
+          <FormControl.Label>Quickbars</FormControl.Label>
+          {parameters?.plugins?.includes("quickbars") ? (
+            <TextInput
+              name="quickbarsSelectionToolbar"
+              id="quickbarsSelectionToolbar"
+              value={parameters.quickbarsSelectionToolbar}
+              onChange={onInputChange}
+            />
+          ) : null}
+
+          <FormControl.Label>Format</FormControl.Label>
+          <TextInput
+            name="formatgroup"
+            id="formatgroup"
+            value={parameters.formatgroup}
+            onChange={onInputChange}
+          />
+          <FormControl.HelpText>
+            Referenced using key 'formatgroup' in the toolbar field
+          </FormControl.HelpText>
+
+          <FormControl.Label>Assets</FormControl.Label>
+          <TextInput
+            name="assetgroup"
+            id="assetgroup"
+            value={parameters.assetgroup}
+            onChange={onInputChange}
+          />
+          <FormControl.HelpText>
+            Handles Contentful assets using `existingasset` for inserting an existing Asset or `newasset` for creating a new Asset. Referenced using key 'assetgroup' in the toolbar field.
+          </FormControl.HelpText>
         </FormControl>
       </Form>
     </Flex>
--- src/locations/Field.tsx
+++ src/locations/Field.tsx
@@ -3,12 +3,27 @@ import { Paragraph } from '@contentful/f36-components';
 import { FieldAppSDK } from '@contentful/app-sdk';
 import { /* useCMA, */ useSDK } from '@contentful/react-apps-toolkit';
 import { Editor } from '@tinymce/tinymce-react';
+import { AppInstallationParameters, defaultParameters } from './ConfigDefaults';
+
+interface ImageProps {
+  id: string;
+  url: string;
+  width: string;
+  height: string;
+  title?: string;
+}
+
+interface LinkProps {
+  id: string;
+  url: string;
+  title: string;
+}
 
 // https://github.com/pauloamgomes/contentful-tinymce-editor/blob/master/src/components/Field.tsx
 // https://www.tiny.cloud/docs/tinymce/latest/table/
 const Field = () => {
   const sdk = useSDK<FieldAppSDK>();
-  const {tinyMceApiKey} = sdk.parameters.installation;
+  const { tinyMceApiKey } = sdk.parameters.installation;
   let debounceInterval: any = false;
   const [init, setInit] = useState(null as any);
   const editorRef = useRef(null as any);
@@ -33,6 +48,196 @@ const Field = () => {
     return sdk.field.onValueChanged(onExternalChange);
   });
 
+  React.useEffect(() => {
+    const {
+      formatgroup,
+      insertgroup,
+      assetgroup,
+      extragroup,
+      plugins,
+      menubar,
+      toolbar,
+      customcolors,
+      custompalette,
+      colormap,
+      quickbarsSelectionToolbar,
+      resizing,
+      customContentStyle,
+    } = sdk.parameters.installation as AppInstallationParameters;
+
+    const renderImage = ({ id, url, width, height, title = "" }: ImageProps) =>
+      `<img
+        src="${url}"
+        width="${width}"
+        height="${height}"
+        style="max-width:300px;height:auto;";
+        alt="{title}"
+        data-contentful-id="${id}"
+        data-original-width="${width}"
+        data-original-height="${height}"
+      />`;
+
+    const renderLink = ({ id, url, title }: LinkProps) =>
+      `<a href="${url}" data-contentful-id="${id}">${title}</a>`;
+
+    const renderContent = (data: any) => {
+      const { assetType, id, title, url, width, height } = data;
+
+      return assetType === "image"
+        ? renderImage({ id, url, width, height, title })
+        : renderLink({ id, url, title });
+    };
+
+    const setupEditor = (editor: any) => {
+      editor.ui.registry.addButton("existingasset", {
+        text: "Link existing",
+        icon: "edit-image",
+        tooltip: "Insert existing Contentful Media Asset",
+        onAction: async () => {
+          await handleSelectAsset().then((data) => {
+            if (data) {
+              editor.insertContent(renderContent(data));
+            }
+          });
+        },
+      });
+
+      editor.ui.registry.addButton("newasset", {
+        text: "Add new and link",
+        icon: "image",
+        tooltip: "Create and Insert Contentful Media Asset",
+        onAction: async () => {
+          await handleCreateAsset().then((data) => {
+            if (data) {
+              editor.insertContent(renderContent(data));
+            }
+          });
+        },
+      });
+
+      editor.ui.registry.addButton("editasset", {
+        text: "Edit media asset",
+        icon: "image",
+        tooltip: "Edit Contentful Media Asset",
+        onAction: async () => {
+          const node = editor.selection.getNode();
+          const parts = node
+            .getAttribute("src")
+            .replace(/(http:|https:)?\/\/.*\.ctfassets\.net\//, "")
+            .replace(`${sdk.ids.space}/`, "")
+            .split("/");
+
+          await handleEditAsset(parts[0]).then((data) => {
+            if (data) {
+              editor.insertContent(renderContent(data));
+            }
+          });
+        },
+      });
+
+      editor.ui.registry.addContextToolbar("editasset", {
+        predicate: (node: any) => {
+          return (
+            node.nodeName.toLowerCase() === "img" &&
+            node.getAttribute("src").includes(`ctfassets.net/${sdk.ids.space}/`)
+          );
+        },
+        items: "editasset",
+        position: "node",
+        scope: "node",
+      });
+    };
+
+    const getAssetData = (asset: any | null) => {
+      if (asset?.fields?.file && asset?.fields?.file[sdk.field.locale]) {
+        const assetType = /^image\/(.*)$/.test(
+          asset.fields.file[sdk.field.locale].contentType
+        )
+          ? "image"
+          : "file";
+
+        const data: any = {
+          assetType,
+          id: asset.sys.id,
+          url: asset.fields.file[sdk.field.locale].url,
+          title: asset.fields.title[sdk.field.locale],
+        };
+
+        if (assetType === "image") {
+          data.width = asset.fields.file[sdk.field.locale].details.image.width;
+          data.height =
+            asset.fields.file[sdk.field.locale].details.image.height;
+        }
+
+        return data;
+      }
+    };
+
+    const handleSelectAsset = async () =>
+      sdk.dialogs.selectSingleAsset().then((asset: any) => getAssetData(asset));
+
+    const handleCreateAsset = async () =>
+      sdk.navigator
+        .openNewAsset({ slideIn: { waitForClose: true } })
+        .then(({ entity }: any) => getAssetData(entity));
+
+    const handleEditAsset = async (id: string) =>
+      await sdk.navigator
+        .openAsset(id, { slideIn: { waitForClose: true } })
+        .then(({ entity }: any) => getAssetData(entity));
+
+    const defaults: any = {
+      height: 1000,
+      menubar,
+      plugins,
+      toolbar: "undo redo | bold italic underline formatgroup | bullist numlist | table assetgroup link | insertgroup | extragroup",
+      image_caption: true,
+      custom_colors: customcolors,
+      toolbar_groups: {},
+      quickbars_selection_toolbar: quickbarsSelectionToolbar,
+      quickbars_insert_toolbar: false,
+      content_style: customContentStyle,
+      setup: (editor: any) => setupEditor(editor),
+    };
+
+    if (custompalette) {
+      defaults.color_map = colormap;
+    }
+
+    if (formatgroup) {
+      defaults.toolbar_groups.formatgroup = {
+        icon: "format",
+        tooltip: "Formatting",
+        items: formatgroup,
+      };
+    }
+
+      defaults.toolbar_groups.assetgroup = {
+        icon: "gallery",
+        tooltip: "Contentful Assets",
+        items: "existingasset newasset",
+      };
+
+    if (insertgroup) {
+      defaults.toolbar_groups.insertgroup = {
+        icon: "plus",
+        tooltip: "Insert",
+        items: insertgroup,
+      };
+    }
+
+    if (extragroup) {
+      defaults.toolbar_groups.extragroup = {
+        icon: "more-drawer",
+        tooltip: "More",
+        items: extragroup,
+      };
+    }
+    console.log(defaults);
+    
+    setInit(defaults);
+  }, [sdk]);
+
   const handleEditorChange = (newValue: string) => {
     if (debounceInterval) {
       clearInterval(debounceInterval);
@@ -43,34 +248,43 @@ const Field = () => {
     }, 500);
   };
 
+  if (!init) {
+    return (
+      <div>
+        Loading editor
+      </div>
+    )
+  }
+
   return (
     <>
-    <Editor
+      <Editor
         apiKey={tinyMceApiKey}
         onInit={(_evt, editor) => {
           if (!editorRef?.current) return;
           editorRef.current = editor;
         }}
         initialValue={value}
-        init={{
-          height: 500,
-          menubar: true,
-          plugins: [
-            'advlist', 'autolink', 'lists', 'link', 'image', 'charmap', 'preview',
-            'anchor', 'searchreplace', 'visualblocks', 'code', 'fullscreen',
-            'insertdatetime', 'media', 'table', 'code', 'help', 'wordcount', 
-            'table'
-          ],
-          toolbar: 'undo redo | ' +
-            'bold italic forecolor | alignleft aligncenter ' +
-            'alignright | bullist numlist outdent indent | ' +
-            'table tableprops | tabledelete | tablerowheader | tablecellprops tablemergecells tablesplitcells' + 
-            'removeformat | help',
-          content_style: 'body { font-family:Helvetica,Arial,sans-serif; font-size:14px }'
-        }}
+        init={init}
+        // init={{
+        //   height: 500,
+        //   menubar: true,
+        //   plugins: [
+        //     'advlist', 'autolink', 'lists', 'link', 'image', 'charmap', 'preview',
+        //     'anchor', 'searchreplace', 'visualblocks', 'code', 'fullscreen',
+        //     'insertdatetime', 'media', 'table', 'code', 'help', 'wordcount',
+        //     'table'
+        //   ],
+        //   toolbar: 'undo redo | ' +
+        //     'bold italic forecolor | alignleft aligncenter ' +
+        //     'alignright | bullist numlist outdent indent | ' +
+        //     'table tableprops | tabledelete | tablerowheader | tablecellprops tablemergecells tablesplitcells' +
+        //     'removeformat | help',
+        //   content_style: 'body { font-family:Helvetica,Arial,sans-serif; font-size:14px }'
+        // }}
         onEditorChange={handleEditorChange}
       />
-      </>
+    </>
   )
 };
 
